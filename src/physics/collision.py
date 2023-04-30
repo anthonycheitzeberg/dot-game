038f463409f0e30137cbee81ee7e6f4dcef2d6f3
robertschelling <robert.schelling@mllrdev.com>
2023-04-30 12:36:10 +0800

    Added basic Shapes

---
+++ src/physics/collision.py
@@ -0,0 +1,29 @@
+class Collision():
+    def __init__(self):
+        self.isCollision = false
+
+
+    def calculateCollision(self, shape1, shape2):
+        if isinstance(shape1, Circle):
+            if isinstance(shape2, Rectangle):
+                pass
+            if isinstance(shape2, Circle):
+                pass
+            if isinstance(shape2, Triangle):
+                pass
+        if isinstance(shape1, Rectangle):
+            if isinstance(shape2, Rectangle):
+                pass
+            if isinstance(shape2, Circle):
+                pass
+            if isinstance(shape2, Triangle):
+                pass
+        if isinstance(shape1, Triangle):
+            if isinstance(shape2, Rectangle):
+                pass
+            if isinstance(shape2, Circle):
+                pass
+            if isinstance(shape2, Triangle):
+                pass
+
+
---
+++ src/physics/physics.py
@@ -0,0 +1,4 @@
+class Physics():
+    def __init__(self):
+        self.dimension = 0
+        self.mass = 0
